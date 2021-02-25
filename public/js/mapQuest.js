401b04c2421caaae40b4223e9564a02b7019875a
Tiffany Ton <t4ton@ucsd.edu>
2021-02-25 10:48:03 -0800

    thirdpartyAPT lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'yjiDcVnRY2RxeDuKgP03DyPMLGZSDEGB';
+
+	var map = L.mapquest.map('map', {
+  		center: [32.8785154,-117.2709055],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+	});
+
+	L.marker([32.8785154,-117.2709055]).addTo(map);
 }
